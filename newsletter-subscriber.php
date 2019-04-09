8a450b8b71d57ca39676d8e7f441e1607bfc75ab
acbrent25 <adamchampagne@gmail.com>
2019-04-09 04:58:24 -0400

    plugin setup

---
+++ newsletter-subscriber.php
@@ -0,0 +1,18 @@
+<?php 
+/*
+Plugin Name: Newsletter Subscriber 
+Description: Form to subscribe to a newsletter
+Version: 1.0
+Author: Adam Champagne
+Auther URI: https://adamchampagne.com/
+*/
+
+ // Exit if Accessed Directly
+ if(!defined('ABSPATH')){
+     exit;
+ }
+
+ // Load Scripts
+ require_once(plugin_dir_path(__FILE__) . '/includes/newsletter-subscriber-scripts.php');
+
+
