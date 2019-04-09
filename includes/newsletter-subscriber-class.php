127d3dce50619b54ce0136baca0fd49b807740b8
acbrent25 <adamchampagne@gmail.com>
2019-04-09 05:03:58 -0400

    widget class

---
+++ includes/newsletter-subscriber-class.php
@@ -0,0 +1,46 @@
+<?php 
+
+class Newsletter_Subscriber_Widget extends WP_Widget {
+
+	/**
+	 * Sets up the widgets name etc
+	 */
+	public function __construct() {
+		$widget_ops = array( 
+			'classname' => 'newsletter_subscriber_widget',
+			'description' => 'Simple email subscirber',
+		);
+		parent::__construct( 'newsletter_subscriber_widget', 'Newsletter Subscriber', $widget_ops );
+	}
+
+	/**
+	 * Outputs the content of the widget
+	 *
+	 * @param array $args
+	 * @param array $instance
+	 */
+	public function widget( $args, $instance ) {
+		// outputs the content of the widget
+	}
+
+	/**
+	 * Outputs the options form on admin
+	 *
+	 * @param array $instance The widget options
+	 */
+	public function form( $instance ) {
+		// outputs the options form on admin
+	}
+
+	/**
+	 * Processing widget options on save
+	 *
+	 * @param array $new_instance The new options
+	 * @param array $old_instance The previous options
+	 *
+	 * @return array
+	 */
+	public function update( $new_instance, $old_instance ) {
+		// processes widget options to be saved
+	}
+}
