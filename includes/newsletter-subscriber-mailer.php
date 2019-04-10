e75ccc071d3695ce9ff36c1f4918131648478636
acbrent25 <adamchampagne@gmail.com>
2019-04-10 06:09:34 -0400

    mailer class

--- includes/newsletter-subscriber-mailer.php
+++ includes/newsletter-subscriber-mailer.php
@@ -1,3 +1,39 @@
 <?php 
 
-echo $_POST['name'];
+// Check if post request has been sent
+
+if($_SERVER['REQUEST_METHOD'] == "POST") {
+    
+    // Get Post Data
+    $name = strip_tags(trim($_POST['name']));
+    $email = filter_var(trim($_POST['email']), FILTER_SANITIZE_EMAIL);
+    $recipient = $_POST['recipient'];
+    $subject = $_POST['subject'];
+
+    // Validation
+    if(empty($name) || empty($email)) {
+        // Send Error
+        http_response_code(400);
+        echo 'Please Fill Out All Fields';
+        exit;
+    }
+
+    // Build Email
+    $message = "Name: $name\n";
+    $message .= "Email: $email\n\n";
+
+    // Build Headers
+    $headers = "From: $name <$email>";
+
+    // Send Email
+    if(mail($recipient, $subject, $message, $headers)){
+        http_response_code(200);
+        echo 'Your are now subscribed';
+    } else {
+        http_response_code(500);
+        echo 'There was a problem';
+    }
+} else {
+    http_response_code(2403);
+    echo 'There was a problem';
+}
